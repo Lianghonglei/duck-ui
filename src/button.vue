7865ba21ff1cc1658349f5759f5ad708a3e9b0d3
lei <895641390@qq.com>
2021-03-08 19:42:32 +0800

    安装当前项目需要的东西报错

---
+++ src/button.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+<script>
+export default {
+  template: `
+  <button class="g-button">按钮</button>
+  `
+}
+
+</script>
+
+<style lang="scss">
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  /*未设定宽度*/
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+
+  &:focus {
+    outline: none;
+  }
+}
+</style>
